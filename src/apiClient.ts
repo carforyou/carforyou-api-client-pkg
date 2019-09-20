599744385dfa8aa350776105a9428d29434307f7
Krzysztof Sakwerda <krzysztof@carforyou.ch>
2019-09-20 11:33:11 +0200

    Configuration (#1)
    * ! forgot @types/jest
    
    * + basic configuration
    
    * ~ version
    
    * + internalize version
    
    * + adjust naming
    
    * + abstraction over exports
    
    * + explicit void return

---
+++ src/apiClient.ts
@@ -0,0 +1,34 @@
+export interface ApiClientConfig {
+  carServiceUrl?: string
+  searchServiceUrl?: string
+  catalogueServiceUrl?: string
+  dealerServiceUrl?: string
+  debug?: boolean
+}
+
+class ApiClient {
+  static instance: ApiClient
+  configuration: ApiClientConfig
+  version: string
+
+  constructor() {
+    this.version = "v1"
+    this.configuration = {}
+    ApiClient.instance = this
+  }
+
+  public configure(configuration: ApiClientConfig): void {
+    if (Object.keys(this.configuration).length) {
+      throw new Error("Owerwriting API client configuration")
+    }
+
+    Object.keys(configuration).forEach(key => {
+      this.configuration[key] = configuration[key]
+    })
+  }
+}
+
+const instance = new ApiClient()
+Object.freeze(instance)
+
+export default instance
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("To be implemented")
+import apiClient from "./apiClient"
+
+export default {
+  configure: apiClient.configure,
+  getConfiguration: () => ({
+    ...apiClient.configuration,
+    version: apiClient.version
+  })
+}
