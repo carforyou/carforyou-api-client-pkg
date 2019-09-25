d4b22d507d3b96415197e8fa57fa82bf91e10e6d
Krzysztof Sakwerda <ksakwerda@gmail.com>
2019-09-25 12:40:59 +0200

    + count with facets

--- src/services/search/listingSearch.ts
+++ src/services/search/listingSearch.ts
@@ -1,13 +1,18 @@
 import { postData, Service } from "../../base"
 
+import { Facets } from "../../types/facets"
 import { SearchParams } from "../../types/params"
 
 export const fetchListingCount = async (
-  query: SearchParams = {}
-): Promise<number> => {
+  query: SearchParams = {},
+  includeFacets = true
+): Promise<{ count: number; facets?: Facets }> => {
   const json = await postData(Service.SEARCH, "listings/count", {
     query
   })
 
-  return json.count
+  return {
+    count: json.count,
+    ...(includeFacets ? { facets: json.facets } : {})
+  }
 }
---
+++ src/types/facets.ts
@@ -0,0 +1,7 @@
+export interface Facet {
+  [key: string]: number
+}
+
+export interface Facets {
+  [key: string]: Facet
+}
