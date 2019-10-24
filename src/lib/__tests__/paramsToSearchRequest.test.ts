80674dc66a450b03247b2a0bb0b72548d04f508e
Christian Felder <masone@users.noreply.github.com>
2019-10-24 15:39:28 +0200

    wrap location params (#25)

---
+++ src/lib/__tests__/paramsToSearchRequest.test.ts
@@ -0,0 +1,40 @@
+import paramsToSearchRequest from "../paramsToSearchRequest"
+
+describe("#paramsToSearchRequest", () => {
+  it("includes regular params", () => {
+    expect(paramsToSearchRequest({ makeKey: ["bmw"] })).toEqual({
+      makeKey: ["bmw"]
+    })
+  })
+
+  describe("location params", () => {
+    it("wrap cityId", () => {
+      expect(paramsToSearchRequest({ cityId: "1001" })).toEqual({
+        location: { cityId: "1001" }
+      })
+    })
+
+    it("strips radius without cityId", () => {
+      expect(paramsToSearchRequest({ radius: "20" })).toEqual({})
+    })
+
+    it("wrap both radius and cityId", () => {
+      expect(paramsToSearchRequest({ radius: "20", cityId: "1001" })).toEqual({
+        location: { radius: "20", cityId: "1001" }
+      })
+    })
+
+    it("persists regular params", () => {
+      expect(
+        paramsToSearchRequest({
+          makeKey: ["bmw"],
+          radius: "20",
+          cityId: "1001"
+        })
+      ).toEqual({
+        makeKey: ["bmw"],
+        location: { radius: "20", cityId: "1001" }
+      })
+    })
+  })
+})
---
+++ src/lib/paramsToSearchRequest.ts
@@ -0,0 +1,17 @@
+import {
+  ListingQueryParams,
+  ListingFilterParams,
+  LocationFilter
+} from "../types/params/listings"
+
+const paramsToSearchRequest = (
+  params: ListingFilterParams | ListingQueryParams
+): object => {
+  const { cityId, radius, ...rest } = params
+  const location: LocationFilter =
+    cityId && radius ? { cityId, radius } : cityId ? { cityId } : undefined
+
+  return { ...rest, ...(location ? { location } : {}) }
+}
+
+export default paramsToSearchRequest
