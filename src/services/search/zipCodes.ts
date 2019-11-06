984b80234710b7a09dddf02b05c24117bbb2b11b
raluca <lacatusu.raluca@gmail.com>
2019-11-06 16:34:06 +0100

    feat: removed zipCodes api

--- src/services/search/zipCodes.ts
+++
@@ -1,7 +0,0 @@
-import { fetchPath, Service } from "../../base"
-
-import { ZipCode } from "../../types/models"
-
-export const fetchZipCodes = (): Promise<ZipCode[]> => {
-  return fetchPath(Service.SEARCH, "/zipcodes")
-}
