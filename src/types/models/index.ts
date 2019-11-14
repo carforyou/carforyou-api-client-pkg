b4df44888783115e6444f33ade0860209ef711b5
rcky <ruefe@rcky.ch>
2019-11-14 11:02:26 +0100

    fix(dealer-name-slug): makes the dealer's nameSlug optional
    Note: Not all endpoints will deliver the nameSlug for the dealer. Therefore it's optional.

--- src/types/models/index.ts
+++ src/types/models/index.ts
@@ -15,7 +15,7 @@ export interface Model extends MappedValue {
 export interface Dealer {
   id: number
   name: string
-  nameSlug: string
+  nameSlug?: string
   phone: string
   email: string
   address: string
