ebafd79b9e94fe7afd308d131920745c3e695c79
Lorenzo Kappeler <lorenzo@carforyou.ch>
2020-01-20 11:15:05 +0100

    chore: add more options to the options factory

--- src/lib/factories/options.ts
+++ src/lib/factories/options.ts
@@ -12,18 +12,92 @@ export function Options(): OptionsType {
             name: "ABS package item (de)"
           }
         ]
+      },
+      {
+        id: 2,
+        name: "Befestigungsösen im Laderaum",
+        packageItems: []
+      },
+      {
+        id: 3,
+        name: "Innenraumlicht-Paket",
+        packageItems: [
+          {
+            id: 4,
+            name: "Innenraumlicht-Paket Option One"
+          },
+          {
+            id: 5,
+            name: "Innenraumlicht-Paket Option Two"
+          },
+          {
+            id: 6,
+            name: "Innenraumlicht-Paket Option Theree"
+          }
+        ]
+      },
+      {
+        id: 7,
+        name: "Getönte Scheiben",
+        packageItems: []
+      },
+      {
+        id: 8,
+        name: "3 Kopfstützen hinten",
+        packageItems: []
       }
     ],
     additionalOptions: [
       {
-        id: 2,
+        id: 10,
         name: "Bluetooth (de)",
         packageItems: [
           {
-            id: 2102,
+            id: 11,
             name: "Bluetooth package item (de)"
           }
         ]
+      },
+      {
+        id: 12,
+        name: "8 Lautsprecher",
+        packageItems: []
+      },
+      {
+        id: 13,
+        name: "Paket Proline",
+        packageItems: [
+          {
+            id: 14,
+            name: "8 Lautsprecher"
+          },
+          {
+            id: 15,
+            name: "Audi hold assist"
+          },
+          {
+            id: 16,
+            name: "Digitaler Radioempfang (DAB)"
+          },
+          {
+            id: 17,
+            name: "Innenspiegel automatisch abblendbar"
+          },
+          {
+            id: 18,
+            name:
+              "Kindersitzvorrichtung ISOFIX mit Deaktivierungsschalter für Beifahrer- Airbag"
+          },
+          {
+            id: 19,
+            name: "Licht- und Regensensor"
+          }
+        ]
+      },
+      {
+        id: 20,
+        name: "Tempomat",
+        packageItems: []
       }
     ]
   }
