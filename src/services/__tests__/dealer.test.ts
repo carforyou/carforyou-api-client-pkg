1b168b91f1a79ac5de460d4af153ae3b5672617f
GutierrezGo <virginia@carforyou.ch>
2020-03-02 11:36:47 +0100

    CAR-3715: tests

--- src/services/__tests__/dealer.test.ts
+++ src/services/__tests__/dealer.test.ts
@@ -1,4 +1,8 @@
-import { fetchDealerSuggestions } from "../dealer"
+import {
+  fetchDealerSuggestions,
+  fetchDealerProfile,
+  putDealerProfile
+} from "../dealer"
 
 describe("Dealer", () => {
   describe("#fetchDealerSuggestions", () => {
@@ -12,6 +16,36 @@ describe("Dealer", () => {
         )}`,
         expect.any(Object)
       )
+    })
+  })
+
+  describe("#fetchDealerProfile", () => {
+    const dealerIdMock = 12
+    const profileMock = {
+      address: "Heiterweit",
+      city: "SW",
+      dealerSource: "dealer",
+      id: dealerIdMock,
+      phone: "12-13-65",
+      zipCode: "345"
+    }
+    beforeEach(() => {
+      fetchMock.mockResponse(JSON.stringify(profileMock))
+    })
+    it("fetch dealer data", async () => {
+      const profile = await fetchDealerProfile(dealerIdMock)
+
+      expect(profile).toEqual(profileMock)
+    })
+
+    it("put dealer data", async () => {
+      fetchMock.mockResponse(JSON.stringify(profileMock))
+      const profileRespone = await putDealerProfile({
+        dealerId: dealerIdMock,
+        profile: profileMock
+      })
+
+      expect(profileRespone.tag).toBe("success")
     })
   })
 })
