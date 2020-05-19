a134917f2c6dc7b87688dc33e4b4245ee4ce6724
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-05-19 15:15:29 +0200

    chore: extend naming convention rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
   extends: ["@carforyou/eslint-config"],
+  rules: {
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "enumMember",
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "variableLike",
+        format: ["camelCase", "PascalCase", "snake_case"],
+        leadingUnderscore: "allow",
+      },
+    ],
+  },
 }
