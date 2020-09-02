35865f9c42c4fc3f9bb7db3219182b818c8ca4e1
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-09-02 15:00:49 +0200

    fix: only pass options if there are any

--- src/services/dealer.ts
+++ src/services/dealer.ts
@@ -21,7 +21,9 @@ export const fetchDealer = async (
 ): Promise<Dealer> => {
   return fetchPath(
     Service.DEALER,
-    `dealers/${id}${options.language ? `?${toQueryString(options)}` : ""}`
+    `dealers/${id}${
+      Object.keys(options).length ? `?${toQueryString(options)}` : ""
+    }`
   )
 }
 
