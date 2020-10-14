3759e0c7e1e55d354b651a43d13f708f6dc908a4
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-10-14 17:15:00 +0200

    feat: refactor current make models

--- src/services/search/currentMakeModels.ts
+++ src/services/search/currentMakeModels.ts
@@ -1,14 +1,22 @@
-import { fetchPath, Service } from "../../base"
+import { fetchPath, RequestOptions } from "../../base"
 
 import { Make, Model } from "../../types/models"
 
-export const fetchCurrentMakes = async (): Promise<Make[]> => {
-  return fetchPath(Service.SEARCH, "current-makes")
+export const fetchCurrentMakes = async ({
+  options = {},
+}: { options?: RequestOptions } = {}): Promise<Make[]> => {
+  return fetchPath({ path: "current-makes", options })
 }
 
-export const fetchCurrentModels = async (makeKey: string): Promise<Model[]> => {
-  return fetchPath(
-    Service.SEARCH,
-    `current-makes/key/${makeKey}/current-models?includeBodyTypes=true`
-  )
+export const fetchCurrentModels = async ({
+  makeKey,
+  options = {},
+}: {
+  makeKey: string
+  options?: RequestOptions
+}): Promise<Model[]> => {
+  return fetchPath({
+    path: `current-makes/key/${makeKey}/current-models?includeBodyTypes=true`,
+    options,
+  })
 }
