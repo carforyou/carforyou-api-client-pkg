87f62e5c5bb9cbe293020c095e5eeb9bcd3c57ab
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-10-14 17:01:49 +0200

    feat: refactor listing options

--- src/services/option/listing.ts
+++ src/services/option/listing.ts
@@ -1,55 +1,72 @@
-import { fetchPath, Service, handleValidationError, putData } from "../../base"
+import {
+  fetchPath,
+  handleValidationError,
+  putData,
+  RequestOptions,
+} from "../../base"
 import { withTokenRefresh } from "../../tokenRefresh"
 
 import { Options } from "../../types/models"
 import { Listing } from "../../types/models/listing"
 import { WithValidationError } from "../../types/withValidationError"
 
-export const fetchListingOptions = async (
-  id: number,
-  lng: string
-): Promise<Options> => {
-  return fetchPath(Service.OPTION, `listings/${id}/options?language=${lng}`)
+export const fetchListingOptions = async ({
+  listingId,
+  locale,
+  options = {},
+}: {
+  listingId: number
+  locale: string
+  options?: RequestOptions
+}): Promise<Options> => {
+  return fetchPath({
+    path: `listings/${listingId}/options?language=${locale}`,
+    options,
+  })
 }
 
 export const fetchDealerListingOptions = async ({
   dealerId,
   listingId,
+  options = {},
 }: {
   dealerId: number
   listingId: number
+  options?: RequestOptions
 }): Promise<Options> => {
   return withTokenRefresh(() =>
-    fetchPath(
-      Service.OPTION,
-      `dealers/${dealerId}/listings/${listingId}/options`
-    )
+    fetchPath({
+      path: `dealers/${dealerId}/listings/${listingId}/options`,
+      options,
+    })
   )
 }
 
 export const saveDealerListingOptions = async ({
   dealerId,
   listing,
+  options = {},
 }: {
   dealerId: number
   listing: Listing
+  options?: RequestOptions
 }): Promise<WithValidationError<Listing>> => {
   const { standardOptions, additionalOptions, id } = listing
 
   return withTokenRefresh(async () => {
     try {
-      await putData(
-        Service.OPTION,
-        `dealers/${dealerId}/listings/${id}/options`,
-        {
+      await putData({
+        path: `dealers/${dealerId}/listings/${id}/options`,
+        body: {
           standardOptions: standardOptions.map((optionId) => ({
             id: optionId,
           })),
           additionalOptions: additionalOptions.map((optionId) => ({
             id: optionId,
           })),
-        }
-      )
+        },
+        options,
+      })
     } catch (error) {
       return handleValidationError(error)
     }
