4884069fd101a481535f4cb54dc8e92e86c9dea0
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-10-14 17:36:52 +0200

    feat: refactor facets calls

--- src/services/search/facets.ts
+++ src/services/search/facets.ts
@@ -1,17 +1,26 @@
-import { postData, Service } from "../../base"
+import { postData, RequestOptions } from "../../base"
 
 import { ListingSearchParams } from "../../types/params/listings"
 import { Facets } from "../../types/facets"
 
 import paramsToSearchRequest from "../../lib/paramsToSearchRequest"
 
-export const fetchFacets = async (
-  query: ListingSearchParams = {},
-  fields: string[] = []
-): Promise<Facets> => {
-  const json = await postData(Service.SEARCH, "/listings/facets", {
-    query: paramsToSearchRequest(query),
-    fields,
+export const fetchFacets = async ({
+  query = {},
+  fields = [],
+  options = {},
+}: {
+  query?: ListingSearchParams
+  fields?: string[]
+  options?: RequestOptions
+} = {}): Promise<Facets> => {
+  const json = await postData({
+    path: "/listings/facets",
+    body: {
+      query: paramsToSearchRequest(query),
+      fields,
+    },
+    options,
   })
 
   return json.facets
