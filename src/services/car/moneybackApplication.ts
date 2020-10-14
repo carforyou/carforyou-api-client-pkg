4ba3a8c30d09d8d953f89cfb0b73ea9eab0e57e2
Krzysztof Sakwerda <ksakwerda@gmail.com>
2020-10-14 16:57:39 +0200

    feat: refactor moneback application

--- src/services/car/moneybackApplication.ts
+++ src/services/car/moneybackApplication.ts
@@ -1,30 +1,47 @@
-import { postData, handleValidationError, Service } from "../../base"
+import {
+  postData,
+  handleValidationError,
+  RequestOptionsWithRecaptcha,
+} from "../../base"
 
 import { WithValidationError } from "../../types/withValidationError"
 import { MoneybackApplication } from "../../types/models/applications"
 
-export const sendMoneybackApplication = async (
-  listingId: number,
-  moneybackApplication: MoneybackApplication,
-  options = {}
-): Promise<WithValidationError<MoneybackApplication>> => {
-  const { validateOnly, recaptchaToken, validateFields } = {
-    validateOnly: false,
-    recaptchaToken: null,
-    validateFields: Object.keys(moneybackApplication),
-    ...options,
+export const sendMoneybackApplication = async ({
+  listingId,
+  moneybackApplication,
+  options = {},
+}: {
+  listingId: number
+  moneybackApplication: MoneybackApplication
+  options: RequestOptionsWithRecaptcha & {
+    validateOnly?: boolean
+    validateFields?: string[]
   }
+}): Promise<WithValidationError<MoneybackApplication>> => {
+  const {
+    validateOnly = false,
+    validateFields = Object.keys(moneybackApplication),
+    ...rest
+  } = options
+  const { headers = {}, ...otherOptions } = rest
+
   const path = `listings/${listingId}/mbg-applications${
     validateOnly ? "/validate" : ""
   }`
 
-  const headers = {
-    ...(recaptchaToken ? { "Recaptcha-Token": recaptchaToken } : {}),
-    "Validate-Fields": validateFields.join(","),
-  }
-
   try {
-    await postData(Service.CAR, path, moneybackApplication, headers)
+    await postData({
+      path,
+      body: moneybackApplication,
+      options: {
+        headers: {
+          ...headers,
+          "Validate-Fields": validateFields.join(","),
+        },
+        ...otherOptions,
+      },
+    })
     return {
       tag: "success",
       result: moneybackApplication,
