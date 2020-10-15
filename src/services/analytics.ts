8275cbb59cb3c372e603173632ef44c84e6ff47a
Lorenzo Kappeler <me@lnk.codes>
2020-10-15 16:10:16 +0200

    fix: add auth data to analytics request

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,16 +1,21 @@
-import { postData, Service } from "../base"
+import { postData, RequestOptions, Service } from "../base"
 
 import { DealerListingsAnalyticsData } from "../types/models"
 
 export const fetchAnalyticsData = async (
   dealerId: number,
-  listingIds: number[]
+  listingIds: number[],
+  options: RequestOptions
 ): Promise<DealerListingsAnalyticsData> => {
   return postData({
     service: Service.ANALYTICS,
     path: `analytics/dealers/${dealerId}/listings/metrics`,
     body: {
       id: listingIds,
+    },
+    options: {
+      isAuthorizedRequest: true,
+      ...options,
     },
   })
 }
