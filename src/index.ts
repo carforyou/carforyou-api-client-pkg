b2a017808129d3d077db4218041b1a2126b577b7
Christian Felder (masone) <ema@rh-productions.ch>
2020-10-28 10:13:27 +0100

    fix: expose call

--- src/index.ts
+++ src/index.ts
@@ -157,6 +157,7 @@ export {
 export { sendMoneybackApplication } from "./services/car/moneybackApplication"
 export { sendBuyNowApplication } from "./services/car/buyNowApplication"
 export { sendMessageLead } from "./services/car/messageLead"
+export { confirmPurchase } from "./services/carSaleTracking/purchaseConfirmation"
 export {
   fetchLeasingFormUrl,
   sendLeasingInterest,
