4b5c743cb6bd48a95dc362addb8e05675b7efe2e
Daniel Mrđanov <danielmrdjanov@gmail.com>
2021-03-22 11:45:39 +0100

    feat: make search query optional

--- src/types/params/leads.ts
+++ src/types/params/leads.ts
@@ -3,5 +3,5 @@ import { LeadSortTypeParams, SortParams } from "../../types/sort"
 import { PaginationParams } from "./index"
 export interface LeadQueryParams extends PaginationParams {
   sort: SortParams<LeadSortTypeParams>
-  searchQuery: string
+  searchQuery?: string
 }
