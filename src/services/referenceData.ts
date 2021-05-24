a097b2307d1cd5ac628eac05d554273d603bca87
ghelshani <gzim.helshanii@gmail.com>
2021-05-24 10:28:53 +0200

    feat(reference_data): strongly typed return type

--- src/services/referenceData.ts
+++ src/services/referenceData.ts
@@ -2,6 +2,22 @@ import { ApiCallOptions, fetchPath } from "../base"
 
 export const fetchReferenceData = ({
   options = {},
-}: { options?: ApiCallOptions } = {}): Promise<string[]> => {
+}: { options?: ApiCallOptions } = {}): Promise<{
+  bodyTypes: string[]
+  conditionTypes: string[]
+  fuelTypes: string[]
+  fuelTypeGroups: string[]
+  transmissionTypes: string[]
+  driveTypes: string[]
+  colors: string[]
+  colorGroups: string[]
+  searchAttributes: string[]
+  lifestyleTypes: string[]
+  vehicleClasses: string[]
+  imageCountGroups: string[]
+  sourceGroups: string[]
+  lastPriceModifiedDateAgeGroups: string[]
+  firstPublishingDateAgeGroups: string[]
+}> => {
   return fetchPath({ path: "reference-data", options })
 }
