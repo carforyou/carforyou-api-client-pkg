1680b09a7849de451e1ced5d59c58ecd6d581123
Lorenzo Kappeler <lorenzo@carforyou.ch>
2021-06-01 19:38:00 +0200

    BREAKING CHANGE: remove deprecated endpoints (#299)
    BREAKING CHANGE: remove deprecated endpoints fetchDoors, fetchMinFirstRegistrationYear, fetchOptions and fetchSeats

--- src/services/car/goodBadDealData.ts
+++
@@ -1,7 +0,0 @@
-import { ApiCallOptions, fetchPath } from "../../base"
-
-export const fetchDealScores = ({
-  options = {},
-}: { options?: ApiCallOptions } = {}): Promise<string[]> => {
-  return fetchPath({ path: "gbd/scores", options })
-}
