61c01e31c565e0702f4d22c67f977057817a52dc
Krzysztof Sakwerda <ksakwerda@gmail.com>
2021-08-04 12:39:24 +0200

    feat: generic type for reference data

--- src/services/referenceData.ts
+++ src/services/referenceData.ts
@@ -3,27 +3,7 @@ import { ApiCallOptions, fetchPath } from "../base"
 export const fetchReferenceData = ({
   options = {},
 }: { options?: ApiCallOptions } = {}): Promise<{
-  bodyTypes: string[]
-  conditionTypes: string[]
-  fuelTypes: string[]
-  fuelTypeGroups: string[]
-  transmissionTypes: string[]
-  driveTypes: string[]
-  colors: string[]
-  colorGroups: string[]
-  searchAttributes: string[]
-  lifestyleTypes: string[]
-  vehicleClasses: string[]
-  imageCountGroups: string[]
-  sourceGroups: string[]
-  lastPriceModifiedDateAgeGroups: string[]
-  firstPublishingDateAgeGroups: string[]
-  minRegistrationYear: number
-  seats: number[]
-  doors: number[]
-  gbdScores: string[]
-  chargePorts: string[]
-  fastChargePorts: string[]
+  [key: string]: number | string[] | number[]
 }> => {
   return fetchPath({ path: "reference-data", options })
 }
