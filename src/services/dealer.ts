1ac3f1a2bb6fc10469a9c812dcc62e1ba08115d6
Ufuk <40758856+ufkaya@users.noreply.github.com>
2021-10-15 09:59:45 +0200

    fix: remove handlevalidation (#364)

--- src/services/dealer.ts
+++ src/services/dealer.ts
@@ -324,18 +324,8 @@ export const deleteUser = async ({
   options = {},
 }: {
   options?: ApiCallOptions
-}): Promise<WithValidationError> => {
-  try {
-    await deletePath({
-      path: `users/me`,
-      options: { isAuthorizedRequest: true, ...options },
-    })
-
-    return {
-      tag: "success",
-      result: {},
-    }
-  } catch (error) {
-    return handleValidationError(error, { swallowErrors: true })
-  }
-}
+}): Promise<Response> =>
+  deletePath({
+    path: `users/me`,
+    options: { isAuthorizedRequest: true, ...options },
+  })
