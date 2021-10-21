9d641771feb8733c60c5711a81f489298c51ab2d
Virginia <37380787+GutierrezGo@users.noreply.github.com>
2021-10-21 16:20:50 +0200

    feat: added validation endpoint in BuyerProtection

--- src/services/car/buyerProtection.ts
+++ src/services/car/buyerProtection.ts
@@ -1,5 +1,6 @@
 import { WithValidationError } from "../../types/withValidationError"
 import { BuyerProtectionApplication } from "../../types/models/buyerProtection"
+import { createApiPathWithValidate } from "../../lib/path"
 import { ApiCallOptions, handleValidationError, postData } from "../../base"
 
 export const sendBuyerProtectionApplication = async ({
@@ -9,11 +10,18 @@ export const sendBuyerProtectionApplication = async ({
 }: {
   listingId: number
   buyerProtectionApplication: BuyerProtectionApplication
-  options?: ApiCallOptions
+  options?: ApiCallOptions & { validateOnly?: boolean }
 }): Promise<WithValidationError<BuyerProtectionApplication>> => {
+  const { validateOnly = false } = options
+
+  const path = createApiPathWithValidate(
+    `/listings/${listingId}/buyer-protection-applications`,
+    validateOnly
+  )
+
   try {
     const response = await postData({
-      path: `listings/${listingId}/buyer-protection-applications`,
+      path: path,
       body: buyerProtectionApplication,
       options,
     })
