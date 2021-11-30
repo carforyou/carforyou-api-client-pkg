b5d981c76ee73eee360154d37cee19f0acff91f6
Lorenzo Kappeler <me@lnk.codes>
2021-11-30 16:42:41 +0100

    fix: add createdDateFrom

--- src/services/reporting/messageLeads.ts
+++ src/services/reporting/messageLeads.ts
@@ -6,14 +6,16 @@ export const fetchMessageLeads = async ({
   offset,
   page,
   size,
+  createdDateFrom,
   options = {},
 }: {
   offset?: number
   page?: number
   size?: number
+  createdDateFrom?: string
   options?: ApiCallOptions
 } = {}): Promise<Paginated<UserMessageLead>> => {
-  const query = toQueryString({ offset, page, size })
+  const query = toQueryString({ offset, page, size, createdDateFrom })
   return fetchPath({
     path: `users/me/message-leads${query ? "?" + query : ""}`,
     options: { isAuthorizedRequest: true, ...options },
