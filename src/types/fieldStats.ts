b06bdae1cdcebf9736967f0e1e6afb45f1c2ba23
Krzysztof Sakwerda <ksakwerda@gmail.com>
2021-12-08 13:02:34 +0100

    chore: format

--- src/types/fieldStats.ts
+++ src/types/fieldStats.ts
@@ -9,6 +9,6 @@ export interface FieldsStats {
   [key: string]: FieldStat
 }
 
-export type WithFieldStats<T extends unknown> = T & {
+export type WithFieldStats<T> = T & {
   fieldsStats?: FieldsStats
 }
