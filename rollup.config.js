402a2a2815d72173b36914ebd07db63b5df2b539
Christian Felder (masone) <ema@rh-productions.ch>
2021-12-10 13:38:44 +0100

    fix: build with rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import dts from "rollup-plugin-dts"
+import typescript from "@rollup/plugin-typescript"
+import commonjs from "@rollup/plugin-commonjs"
+
+import packageJson from "./package.json")
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: "react-lib",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [commonjs(), typescript()],
+  },
+  {
+    input: "./pkg/esm/types/index.d.ts",
+    output: [{ file: packageJson.types, format: "esm" }],
+    plugins: [dts()],
+  },
+]
