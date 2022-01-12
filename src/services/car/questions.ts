b21f06402b8cd03d471052c69590bd316a635d17
talamcol <talamcol>
2022-01-12 08:48:46 +0100

    feat: add pagination support

--- src/services/car/questions.ts
+++ src/services/car/questions.ts
@@ -1,5 +1,14 @@
+import { ListingQuestionsSortParams } from "../../types/sort"
 import { Paginated } from "../../types/pagination"
-import { PaginatedFactory, WithValidationError } from "../../index"
+import toQueryString from "../../lib/toQueryString"
+import toCamelCase from "../../lib/toCamelCase"
+import { pageOrDefault, sizeOrDefault } from "../../lib/pageParams"
+import {
+  PaginationParams,
+  SortOrderParams,
+  SortParams,
+  WithValidationError,
+} from "../../index"
 import {
   ApiCallOptions,
   fetchPath,
@@ -15,58 +24,38 @@ export interface Question {
   answerDate: Date
 }
 
-const mockQuestions: Question[] = [
-  {
-    id: "123",
-    question: "When was the last service?",
-    createdDate: new Date("2022-01-07"),
-    answer: "We will do another service before you pick up the vehicle",
-    answerDate: new Date("2022-01-11"),
-  },
-  {
-    id: "1234567",
-    question:
-      "What is the color of the car? I'm now just adding text to make the question longer to see how that looks like on the UI. Don't mind this part of the question please.",
-    createdDate: new Date("2022-01-07"),
-    answer:
-      "The car is red my friend. I'm now just adding text to make the question longer to see how that looks like on the UI. Don't mind this part of the question please. WUHUUUUUUUU!!!",
-    answerDate: new Date("2022-01-09"),
-  },
-  {
-    id: "1234568",
-    question: "Where do you live?",
-    createdDate: new Date("2022-01-07"),
-    answer: "I live in Geneva",
-    answerDate: new Date("2022-01-09"),
-  },
-  {
-    id: "1234569",
-    question: "Why can I not travel to Australia?",
-    createdDate: new Date("2022-01-07"),
-    answer: "Because you are not vaccinated",
-    answerDate: new Date("2022-01-09"),
-  },
-  {
-    id: "1234561",
-    question: "Can I win now since Djokovic is not here?",
-    createdDate: new Date("2022-01-07"),
-    answer: "I don't know, we will see",
-    answerDate: new Date("2022-01-09"),
-  },
-]
+interface ListingQuestionsQueryParams extends PaginationParams {
+  sort?: SortParams<ListingQuestionsSortParams>
+}
 
 export const fetchListingQuestions = async ({
   listingId,
+  query,
   options = {},
 }: {
   listingId: number
+  query?: ListingQuestionsQueryParams
   options?: ApiCallOptions
 }): Promise<Paginated<Question>> => {
-  return Promise.resolve(PaginatedFactory(mockQuestions))
-  /*return fetchPath({
-    path: `listings/${listingId}/questions`,
+  const sortOrDefault = {
+    sortType: query?.sort?.sortType || ListingQuestionsSortParams.CREATED_DATE,
+    sortOrder: query?.sort?.sortOrder || SortOrderParams.DESC,
+  }
+  const defaultPagination = {
+    page: 0,
+    size: 2,
+  }
+  const queryParams = {
+    page: pageOrDefault(query?.page, defaultPagination),
+    size: sizeOrDefault(query?.size, defaultPagination),
+    sort: `${toCamelCase(sortOrDefault.sortType)},${toCamelCase(
+      sortOrDefault.sortOrder
+    )}`,
+  }
+  return fetchPath({
+    path: `listings/${listingId}/questions?${toQueryString(queryParams)}`,
     options: options,
-  })*/
+  })
 }
 
 export const createQuestion = async ({
