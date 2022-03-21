1cad51c6066ab4d47213c0ecc76d0f03e9c7192d
Marine-Berthier <52455155+Marine-Berthier@users.noreply.github.com>
2022-03-21 15:25:01 +0100

    fix: update links in readme and remove unused apis
    fix: update links in readme and remove unused apis

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,8 +1,4 @@
-import {
-  CockpitAnalytics,
-  DealerAnalytics,
-  DealerListingsAnalyticsData,
-} from "../types/models"
+import { DealerAnalytics, DealerListingsAnalyticsData } from "../types/models"
 import { ApiCallOptions, postData } from "../base"
 
 export const fetchAnalyticsData = async ({
@@ -18,72 +14,6 @@ export const fetchAnalyticsData = async ({
     path: `analytics/dealers/${dealerId}/listings/metrics`,
     body: {
       id: listingIds,
-    },
-    options: {
-      isAuthorizedRequest: true,
-      ...options,
-    },
-  })
-}
-
-export const fetchListingsAnalytics = async ({
-  dealerId,
-  dimensions,
-  options = {},
-}: {
-  dealerId: number
-  dimensions?: string[]
-  options?: ApiCallOptions
-}): Promise<Array<CockpitAnalytics>> => {
-  return postData({
-    path: `dealers/${dealerId}/listings/analytics`,
-    body: {
-      function: "count",
-      dimensions,
-    },
-    options: {
-      isAuthorizedRequest: true,
-      ...options,
-    },
-  })
-}
-
-export const fetchLeadsAnalytics = async ({
-  dealerId,
-  dimensions,
-  options = {},
-}: {
-  dealerId: number
-  dimensions?: string[]
-  options?: ApiCallOptions
-}): Promise<Array<CockpitAnalytics>> => {
-  return postData({
-    path: `dealers/${dealerId}/leads/analytics`,
-    body: {
-      function: "count",
-      dimensions,
-    },
-    options: {
-      isAuthorizedRequest: true,
-      ...options,
-    },
-  })
-}
-
-export const fetchLeadsInteractionsAnalytics = async ({
-  dealerId,
-  dimensions,
-  options = {},
-}: {
-  dealerId: number
-  dimensions?: string[]
-  options?: ApiCallOptions
-}): Promise<Array<CockpitAnalytics>> => {
-  return postData({
-    path: `dealers/${dealerId}/lead-interactions/analytics`,
-    body: {
-      function: "count",
-      dimensions,
     },
     options: {
       isAuthorizedRequest: true,
